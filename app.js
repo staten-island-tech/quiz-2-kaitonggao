531acb6514019614f601ce0ae5f3e0379dbdcc62
kaitonggao <50878288+kaitonggao@users.noreply.github.com>
2019-09-18 11:46:00 -0400

    quiz3 did not work

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-/*John and Mike both play basketball in different teams. In the latest 3 games, John's team scored 89, 120 and 103 points, while Mike's team scored 116, 94 and 123 points.
+/* John and Mike both play basketball in different teams. In the latest 3 games, John's team scored 89, 120 and 103 points, while Mike's team scored 116, 94 and 123 points.
 
 1. Calculate the average score for each team
 2. Decide which teams wins in average (highest average score), and print the winner to the console. Also include the average score in the output.
 3. Then change the scores to show different winners. Don't forget to take into account there might be a draw (the same average score)
 */
 
-/*
+/* Variables:
+
 let Johnscores = [89,120,103]
 let Mike scores = [116,94,123];
 let averageJohn = (teamJohn[0] + teamJohn[1] + teamJohn[2]) / 3;
-let averageMike = (teamMike[0] + teamMike[1] + teamMike[2]) / 3;*/
+let averageMike = (teamMike[0] + teamMike[1] + teamMike[2]) / 3;
+
+Don't set 6 variables for each numbers, just find the average:
+let Johnaverage = (89 + 120 + 103) /3
+let Mikeaverage = (116 + 94 + 123) /3
+
 
 let Johngame1 = 89;
 let Johngame2 = 120;
@@ -37,3 +43,29 @@ else {
     console.log (`John has a greater average, ${Johnaverage}, than Mike.`);
 }
 
+ */
+
+ ///this. = takes things stated in the Object
+ 
+const rob = {
+    fullname: 'Robert Grande', mass: 78, height: 1.69 ,
+    calculateBMI: function(){
+        this.BMI = this.mass/ (this.height * this.height);
+      return this.BMI
+    }
+  }
+  
+  const dejon = {
+     fullname: 'Dejon Curtie', mass: 110, height: 1.95 ,
+     calculateBMI: function(){
+        this.BMI = this.mass/ (this.height * this.height);
+      return this.BMI
+
+    }
+  }
+  
+  
+  console.log(rob.calculateBMI());
+  console.log(dejon.calculateBMI());
+
+  
